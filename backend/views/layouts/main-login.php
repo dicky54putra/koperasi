d4004aa658d250db522c02fcb012a9225914a448
dicky54putra <saputradicky705@gmail.com>
2020-10-02 09:57:02 +0700

    offline image

--- backend/views/layouts/main-login.php
+++ backend/views/layouts/main-login.php
@@ -22,7 +22,7 @@ dmstr\web\AdminLteAsset::register($this);
     <link rel="stylesheet" href="sweetalert2/dist/sweetalert2.min.css">
     <style type="text/css">
         .login-page {
-            background-image: url('https://pict.sindonews.net/dyn/620/jateng/news/2019/06/26/1/6401/tni-miliki-7-alutsista-canggih-yang-bikin-negara-lain-segan-rjm.jpg');
+            background-image: url('images/pst.jpg');
             background-size: cover;
         }
 
